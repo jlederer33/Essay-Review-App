3cb45c981cb01eb04cd877346e4875ec7b2f127f
Justin Lederer <jlederer@sp309-171.cs.vassar.edu>
2023-11-14 20:50:42 -0500

    modified gradle files, now binding works

--- astudio/app/src/main/java/com/example/astudio/MainView.java
+++ astudio/app/src/main/java/com/example/astudio/MainView.java
@@ -1,14 +1,22 @@
 package com.example.astudio;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
-
+import com.example.astudio.databinding.MainBinding;
 import android.view.View;
 
 public class MainView implements IMainView {
 
     FragmentManager fragManager;
+
+    MainBinding binding;
+
+    public MainView(FragmentActivity activity){
+        this.fragManager = activity.getSupportFragmentManager();
+        this.binding = MainBinding.inflate(activity.getLayoutInflater());
+    }
 
     @Override
     public View getRootView() {
