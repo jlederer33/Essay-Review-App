98465668a45cd729caeaf440818858065630ebe9
Tajbiha Faisal <tfaisal@sp309-190.cs.vassar.edu>
2023-11-17 19:05:09 -0500

    Added the unit test.

---
+++ astudio/app/src/test/java/com/example/astudio/Model/DashboardsTest.java
@@ -0,0 +1,40 @@
+package com.example.astudio.Model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import java.util.List;
+
+
+public class DashboardsTest extends TestCase {
+
+        private Dashboards dashboard;
+
+        @Before
+
+        public void setUp() {
+            dashboard = new Dashboards();
+        }
+
+        @Test
+        public void testAddToEssayList() {
+            String title = "Test Title";
+            String text = "Test Text";
+            String type = "Test Type";
+
+
+            dashboard.addToEssayList(title, text, type);
+
+            assertFalse("The essay list should not be empty", Dashboards.essayList.isEmpty());
+            assertEquals("Essay list should have one essay", 1, Dashboards.essayList.size());
+
+            Essay addedEssay = Dashboards.essayList.get(0);
+            assertEquals("Essay list should match", title, addedEssay.getTitle());
+            assertEquals("Essay list should match", text, addedEssay.getText());
+            assertEquals("Essay list should match", type, addedEssay.getType());
+
+        }
+    }
+
+
